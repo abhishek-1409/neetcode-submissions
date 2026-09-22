c6f3b422db2bbd90b59b4ae225c3f5b96031c4d4
Abhishek Sahu <86314561+abhishek-1409@users.noreply.github.com>
2026-09-22 21:23:43 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int [] letter = new int[26];
+        for(int i=0; i<s.length(); i++){
+            letter[s.charAt(i)-'a']++;
+        }
+        for(int i=0; i<t.length(); i++)
+            letter[t.charAt(i)-'a']--;
+        
+        for(int i=0; i<26; i++){
+            if(letter[i] != 0)
+                return false;
+        }
+        return true;
+    }
+}
